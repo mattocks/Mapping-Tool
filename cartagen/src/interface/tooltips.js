1f3cceefe44a2601c4d93df54ef582780c961f21
Benjamin Mattocks <mattocks@whatsupserver.(none)>
2010-08-20 15:07:57 -0400

    reorganized directories, about to merge

--- cartagen/src/interface/tooltips.js
+++
@@ -1,8 +0,0 @@
-/**
- * Contains tooltips and instructions for maps.
- * This file will eventually load all tooltip descriptions and instructions.
- * Will have support for multiple languages.
- */
-
-Tooltips = {
-}
